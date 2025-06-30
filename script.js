3495a4f929c878ab460471cb982d4f41e0192efd
yasminbueno2009 <y.albuquerque@escola.pr.gov.br>
2025-06-30 12:16:24 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+  const texto = document.querySelector("#entrada-de-texto").value;
+
+  const campoResultado = document.querySelector("#resultado-palavrachave");
+
+  const palavrasChave = processaTexto(texto);
+
+  campoResultado.textContent = palavrasChave.join(", ");
+}
+
+function processaTexto(texto) {
+  let palavras = texto.split(/\P{L}+/u);
+
+  return palavras;
+}
